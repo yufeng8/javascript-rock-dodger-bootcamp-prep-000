d8b2575c1d6c1ee7c8e26317e376b1a6de3755e8
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-15 02:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function createRock(x) {
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
      */
-    else if (positionToInteger(rock.style.top) < 400) {
+    else if (top < 400) {
       moveRock()
     }
     else {
